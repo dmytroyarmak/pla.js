389119fefd21d506ffb9ec5d8430313bc63eb363
Dmytro Yarmak <dmytroy@backbase.com>
2015-10-18 18:48:31 +0300

    Implement Gaussian elimination

--- src/plalib.js
+++ src/plalib.js
@@ -1,5 +1,29 @@
+/* jshint esnext: true */
+
 (function(root) {
   'use strict';
 
-  root.plalib = {};
+  function gaussianElimination (m, n, a, b) {
+    var kMax, k, i, j, coef;
+
+    kMax =  Math.min(m, n);
+
+    for (k = 0; k < kMax; k += 1) {
+      for (i = k + 1; i < m; i += 1) {
+        coef = a[i * n + k] / a[k * n + k];
+
+        a[i * n + k] = 0;
+
+        for (j = k + 1; j < n; j += 1) {
+          a[i * n + j] = a[i * n + j] - a[k * n + j] * coef;
+        }
+
+        b[i] = b[i] - b[k] * coef;
+      }
+    }
+  }
+
+  root.plalib = {
+    gaussianElimination: gaussianElimination
+  };
 }(this));
